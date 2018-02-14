d899fab0c0a81ceb5c6dc376af0f34eb4dc997eb
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-14 14:42:29 +0000

    Challenge 10 finished, switching roles

--- lib/link.rb
+++ lib/link.rb
@@ -2,23 +2,11 @@ require 'pg'
 
 class Link
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = connection.exec("SELECT * FROM links")
+    result = DatabaseConnection.query("SELECT * FROM links")
     result.map { |link| link['url'] }
   end
 
   def self.add_new_link(new_link)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    add_link_to_database = connection.exec("INSERT INTO links(url) VALUES('#{new_link}')")
+    add_link_to_database = DatabaseConnection.query("INSERT INTO links(url) VALUES('#{new_link}')")
   end
 end
